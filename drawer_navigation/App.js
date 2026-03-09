f50900a793d0a570a1446e47ae4ab8201da54ffe
Keni Hernandez <kenihernandez.sh@gmail.com>
2026-03-09 15:19:05 -0600

    feat(auth): create bottom tabs for authentication

--- drawer_navigation/App.js
+++ drawer_navigation/App.js
@@ -1,9 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { useState } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 function UsersStackScreen() {
   return (
@@ -21,13 +24,55 @@ function WeatherStackScreen() {
   );
 }
 
+function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Login Screen</Text>
+    </View>
+  );
+}
+
+function SignupScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Sign Up Screen</Text>
+    </View>
+  );
+}
+
 export default function App() {
+  const [auth, setAuth] = useState(false);
+
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Users" component={UsersStackScreen} />
-        <Drawer.Screen name="Weather" component={WeatherStackScreen} />
-      </Drawer.Navigator>
+      {auth ? (
+        <Drawer.Navigator>
+          <Drawer.Screen name="Users" component={UsersStackScreen} />
+          <Drawer.Screen name="Weather" component={WeatherStackScreen} />
+        </Drawer.Navigator>
+      ) : (
+        <Tab.Navigator
+          initialRouteName="Login"
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name == "Login") {
+                iconName = focused ? "log-in" : "log-in-outline";
+              } else if (route.name == "Sign Up") {
+                iconName = focused ? "person-add" : "person-add-outline";
+              }
+
+              return <Ionicons name={iconName} size={24} color={color} />;
+            },
+            tabBarActiveTintColor: "purple",
+            tabBarInactiveTintColor: "gray",
+          })}
+        >
+          <Tab.Screen name="Login" component={LoginScreen} />
+          <Tab.Screen name="Sign Up" component={SignupScreen} />
+        </Tab.Navigator>
+      )}
     </NavigationContainer>
   );
 }
@@ -35,8 +80,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
